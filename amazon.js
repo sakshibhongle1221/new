646caeffdde068e495378035f42fcb9b2a713abd
newRepo <sakshibhongle1221@gamil.com>
2025-04-29 18:59:00 +0530

    use modules and import cart from cart.js

--- amazon.js
+++ amazon.js
@@ -1,4 +1,4 @@
-
+import { cart } from "./data/cart.js";
 let productsHTML = '';
 
 products.forEach((product)=>{
@@ -79,7 +79,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
     cart.forEach((item) => {
       cartQuantity+= item.quantity;
     });
-
+     
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
   });
