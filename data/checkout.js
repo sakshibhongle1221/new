49e5c48ed4680f1b2ba555df04744ba39148bb15
newRepo <sakshibhongle1221@gamil.com>
2025-05-18 23:52:59 +0530

    Generate HTML for order summary

--- data/checkout.js
+++ data/checkout.js
@@ -1,6 +1,6 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import { formatCurrency} from './data/money.js';
+import {formatCurrency} from './data/money.js';
 
 let cartSummaryHTML = '';
 
